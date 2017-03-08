a7619e2bc671177410a1ac5e1d8f55fcd522998e
Teerapuch <teerapuch@hotmail.com>
2017-03-08 16:29:38 +0700

    update .htaccess

--- header.php
+++ header.php
@@ -3,12 +3,14 @@
     <head>
         <meta charset="utf-8">
         <title>Update Modal</title>
+        <link rel="stylesheet" href="assets/css/main.css" charset="utf-8">
         <link rel="stylesheet" href="assets/css/bootstrap.css" charset="utf-8">
         <link rel="stylesheet" href="assets/css/font-awesome.css" charset="utf-8">
         <link rel="stylesheet" href="assets/plugins/datatable/css/jquery.dataTables.css" charset="utf-8">
         <link rel="stylesheet" href="assets/plugins/datatable/css/bootstrap.datatable.css" charset="utf-8">
-        <style media="screen">
-            a:hover{ cursor: pointer; }
-        </style>
+        <?php
+        // config
+        $base_url = '';
+        ?>
     </head>
     <body>
